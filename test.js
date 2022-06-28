17b1ab1dbcd41536a92291abfb3114f88b388277
bright791 <108144213+bright791@users.noreply.github.com>
2022-06-28 09:17:52 +0900

    Update test.js

--- test.js
+++ test.js
@@ -53,7 +53,9 @@ window.onload = function(event){
 
     });
   });
-  
+  document.documentElement.addEventListener('touchstart', function (event) {if (event.touches.length > 1) { event.preventDefault(); } }, false); 
+  var lastTouchEnd = 0; 
+  document.documentElement.addEventListener('touchend', function (event) { var now = (new Date()).getTime(); if (now - lastTouchEnd <= 300) { event.preventDefault(); } lastTouchEnd = now; }, false);
 
   
  
